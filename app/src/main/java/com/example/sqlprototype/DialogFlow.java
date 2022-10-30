f839f51c8f2e7e3c21a25826b78f8e330ebeaaaf
briansenas <briansena23@hotmail.com>
2022-10-30 19:49:30 +0100

    NavigationBar
    Refactor -> Fragment creation, new app style and fixes

---
+++ app/src/main/java/com/example/sqlprototype/DialogFlow.java
@@ -0,0 +1,23 @@
+package com.example.sqlprototype;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DialogFlow extends Fragment {
+
+    View root;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        root = inflater.inflate(R.layout.fragment_dialog_flow, container, false);
+
+        return root;
+    }
+}
--- app/src/main/java/com/example/sqlprototype/MainActivity.java
+++ app/src/main/java/com/example/sqlprototype/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.sqlprototype;
 
 import android.Manifest;
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Typeface;
@@ -12,6 +13,7 @@ import android.speech.SpeechRecognizer;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -29,63 +31,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 import java.util.ArrayList;
 import java.util.Locale;
 
-class Group {
-    EditText txt;
-    ImageView btn;
-    TextView lbl;
-
-    public Group(EditText txt, ImageView btn, TextView lbl) {
-        this.txt = txt;
-        this.btn = btn;
-        this.lbl = lbl;
-    }
-}
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    HomeFragment homeFragment = new HomeFragment();
+    DialogFlow dialogFlow = new DialogFlow();
     public static final Integer RecordAudioRequestCode = 1;
-    Button btnSubmit;
-    EditText txtCarrera, txtAsignatura, txtConvocatoria;
-    ImageView btnCarrera, btnAsignatura, btnConvocatoria;
-    TextView lblCarrera, lblAsignatura, lblConvocatoria;
-    Group currentGroup;
-    Group[] groups;
-    TextView Q_res;
-    private TextToSpeech textToSpeechEngine;
-    private Button ttsButton;
 
-    SpeechRecognizer speechRecognizer;
-
-    int getColor(int color, double transparency_perc) {
-        assert 0 <= transparency_perc && transparency_perc <= 1;
-        color &= 0xFFFFFF; // remove transparency if it has it
-        int transparency = (int)(transparency_perc*255);
-        return (transparency << (8*3)) | color;
-    }
-
-    void setGroupTransparency(Group group, double perc) {
-        group.txt.setTextColor(getColor(R.color.black, perc));
-        group.txt.setHintTextColor(getColor(R.color.black, Math.max(perc-0.2, 0)));
-        group.lbl.setTextColor(getColor(R.color.black, perc));
-        group.btn.setAlpha((float) perc);
-    }
-
-    void textToSpeechGroup(Group group){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            textToSpeechEngine.speak(group.lbl.getText(), TextToSpeech.QUEUE_FLUSH, null, "tts1");
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == RecordAudioRequestCode && grantResults.length > 0 ){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this,"Permission Granted",Toast.LENGTH_SHORT).show();
         }
     }
 
-    void setFocusToCurrentGroup() {
-        for (Group other_group : groups) {
-            if (other_group != currentGroup) {
-                setGroupTransparency(other_group, 0.4);
-            }
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.RECORD_AUDIO},RecordAudioRequestCode);
         }
-        setGroupTransparency(currentGroup, 1);
-        textToSpeechGroup(currentGroup);
     }
 
     @Override
@@ -96,196 +68,27 @@ public class MainActivity extends AppCompatActivity {
             checkPermission();
         }
 
-        btnSubmit = this.findViewById(R.id.btnSubmit);
-        txtCarrera = this.findViewById(R.id.txtCarrera);
-        txtAsignatura = this.findViewById(R.id.txtAsignatura);
-        txtConvocatoria = this.findViewById(R.id.txtConvocatoria);
-        btnCarrera = this.findViewById(R.id.btnCarrera);
-        btnAsignatura = this.findViewById(R.id.btnAsignatura);
-        btnConvocatoria = this.findViewById(R.id.btnConvocatoria);
-        lblCarrera = this.findViewById(R.id.lblCarrera);
-        lblAsignatura = this.findViewById(R.id.lblAsignatura);
-        lblConvocatoria = this.findViewById(R.id.lblConvocatoria);
-        Q_res = this.findViewById(R.id.Q_res);
-
-        textToSpeechEngine = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener(){
             @Override
-            public void onInit(int status) {
-                if (status != TextToSpeech.SUCCESS) {
-                    Log.e("TTS", "Inicio de la síntesis fallido");
+            public boolean onNavigationItemSelected(MenuItem item){
+                switch(item.getItemId()){
+                    case R.id.menuHome:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+                        return true;
+                    case R.id.menuDialog:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,dialogFlow).commit();
+                        return true;
+                    case R.id.mapa:
+                        Toast.makeText(getApplicationContext(),"[SORRY]: This is not implemented yet!",Toast.LENGTH_SHORT).show();
+                        return true;
                 }
+                return false;
             }
         });
 
-        Group grpCarrera = new Group(txtCarrera, btnCarrera, lblCarrera);
-        Group grpAsignatura = new Group(txtAsignatura, btnAsignatura, lblAsignatura);
-        Group grpConvocatoria = new Group(txtConvocatoria, btnConvocatoria, lblConvocatoria);
-        groups = new Group[]{grpCarrera, grpAsignatura, grpConvocatoria};
-        currentGroup = grpCarrera;
-        setFocusToCurrentGroup();
-
-        final Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-
-        btnSubmit.setEnabled(false);
-
-        // Set listeners to each btn, lbl and txt of each group
-        for (Group group : groups) {
-            group.btn.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v) {
-                    currentGroup = group;
-                    setFocusToCurrentGroup();
-
-                    for (Group other_group : groups) {
-                        if (other_group != group) {
-                            other_group.btn.setImageResource(R.drawable.ic_mic_black_off);
-                        }
-                    }
-                    group.btn.setImageResource(R.drawable.ic_mic_black_24dp);
-
-                    speechRecognizer.cancel();
-                    speechRecognizer.startListening(speechRecognizerIntent);
-                }
-            });
-
-            group.lbl.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    currentGroup = group;
-                    setFocusToCurrentGroup();
-                }
-            });
-
-            group.txt.setOnFocusChangeListener(new View.OnFocusChangeListener(){
-                @Override
-                public void onFocusChange(View v, boolean focus) {
-                    if (focus) {
-                        currentGroup = group;
-                        setFocusToCurrentGroup();
-                    }
-                }
-            });
-
-            group.txt.addTextChangedListener(new TextWatcher() {
-                @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-
-                @Override
-                public void onTextChanged(CharSequence s, int start, int before, int count) {
-                    boolean all_filled = true;
-                    for (Group group : groups) {
-                        if (String.valueOf(group.txt.getText()).isEmpty()) {
-                            all_filled = false;
-                        }
-                    }
-                    btnSubmit.setEnabled(all_filled);
-                }
-
-                @Override
-                public void afterTextChanged(Editable editable) {}
-            });
-        }
-
-        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
-        speechRecognizer.setRecognitionListener(new RecognitionListener() {
-            @Override
-            public void onReadyForSpeech(Bundle bundle) {}
-
-            @Override
-            public void onBeginningOfSpeech() {}
-
-            @Override
-            public void onRmsChanged(float v) {}
-
-            @Override
-            public void onBufferReceived(byte[] bytes) {}
-
-            @Override
-            public void onEndOfSpeech() {}
-
-            @Override
-            public void onError(int i) {
-                currentGroup.btn.setImageResource(R.drawable.ic_mic_black_off);
-                currentGroup.txt.setText("Error: " + Integer.toString(i));
-            }
-
-            @Override
-            public void onResults(Bundle bundle) {
-                currentGroup.btn.setImageResource(R.drawable.ic_mic_black_off);
-                ArrayList<String> data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-                currentGroup.txt.setText(data.get(0));
-            }
-
-            @Override
-            public void onPartialResults(Bundle bundle) {}
-
-            @Override
-            public void onEvent(int i, Bundle bundle) {}
-        });
-
-        // Listener to make request to the DB
-        btnSubmit.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                String asign = String.valueOf(txtAsignatura.getText());
-                String carr = String.valueOf(txtCarrera.getText());
-                String conv = String.valueOf(txtConvocatoria.getText());
-                asign = asign.toLowerCase();
-                carr = carr.toLowerCase();
-                conv = conv.toLowerCase();
-                try{
-                    DataBaseAccess db = DataBaseAccess.getInstance(getBaseContext());
-                    db.open();
-                    String resultado = db.getDate(asign,carr,conv);
-
-                    String intro = "Tendrás el examen el";
-                    if(resultado.isEmpty()){
-                        intro = "Lo siento, no hemos encontrado ningún examen";
-                    }
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                        textToSpeechEngine.speak(intro + resultado, TextToSpeech.QUEUE_FLUSH, null, "tts1");
-                    }
-
-                    for (Group group : groups) {
-                        group.txt.setText("");
-                    }
-
-                    // Assign adapter to ListView
-                    Q_res.setText(intro + resultado);
-                    Toast.makeText(MainActivity.this,"Query Submitted",Toast.LENGTH_SHORT).show();
-                    db.close();
-                }
-                catch (Exception e){
-                    Toast.makeText(MainActivity.this,"There's been an Error",Toast.LENGTH_SHORT).show();
-                  
-                }
-            }
-        });
-
-
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        speechRecognizer.destroy();
-    }
-
-    private void checkPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.RECORD_AUDIO},RecordAudioRequestCode);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == RecordAudioRequestCode && grantResults.length > 0 ){
-            if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                Toast.makeText(this,"Permission Granted",Toast.LENGTH_SHORT).show();
-        }
-    }
 
 }
