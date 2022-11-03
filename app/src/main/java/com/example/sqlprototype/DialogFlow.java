6e6bc46a9c5be764f6cc22a2abfe7983dbe5eb80
David Mateos Romero <davidmateos@correo.ugr.es>
2022-11-03 21:13:09 +0100

    Añadido dialogflow con bot de ejemplo (falta cambiarlo)

--- app/src/main/java/com/example/sqlprototype/DialogFlow.java
+++ app/src/main/java/com/example/sqlprototype/DialogFlow.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 public class DialogFlow extends Fragment {
 
@@ -17,6 +18,17 @@ public class DialogFlow extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         root = inflater.inflate(R.layout.fragment_dialog_flow, container, false);
+
+        WebView webview = root.findViewById(R.id.webview);
+        webview.getSettings().setJavaScriptEnabled(true);
+        String html =
+                "<iframe\n" +
+                        "    allow=\"microphone;\"\n" +
+                        "    width=\"100%\"\n" +
+                        "    height=\"100%\"\n" +
+                        "    src=\"https://console.dialogflow.com/api-client/demo/embedded/e3930d62-6d4d-4af5-ac14-80bd69a26d17\">\n" +
+                        "</iframe>";
+        webview.loadData(html, "text/html", null);
 
         return root;
     }
