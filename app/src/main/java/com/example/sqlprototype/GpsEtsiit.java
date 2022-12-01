3f0392fc8d3a4f93337c2baaaf8c3d51abb9a8b8
briansenas <briansena23@hotmail.com>
2022-12-01 12:16:32 +0100

    Pattern 2 Defined

--- app/src/main/java/com/example/sqlprototype/GpsEtsiit.java
+++ app/src/main/java/com/example/sqlprototype/GpsEtsiit.java
@@ -19,6 +19,7 @@ import android.widget.Toast;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class GpsEtsiit extends Fragment implements SensorEventListener {
 
@@ -31,16 +32,23 @@ public class GpsEtsiit extends Fragment implements SensorEventListener {
     SensorManager sensorManager;
     Sensor sensor, sensorgyroscope;
 
-    public static float THRESHOLD_Z = 10f;
-    public static float THRESHOLD_Y = 10f;
+    public static AtomicInteger THRESHOLD_Z = new AtomicInteger(10);
+    public static AtomicInteger THRESHOLD_Y = new AtomicInteger(10);
+    public static float THRESHOLD_GY = 3.5f;
     public float EPSILON_Z = 3f;
     public float EPSILON_Y = 3f;
+    public AtomicInteger orientation_change = new AtomicInteger(0);
     public boolean first_read = false;
     public float FCX, FCY, FCZ;
+    public float lastX,lastY,lastZ = 0;
+    public float EPSILON_O = 0.05f;
     public int SIGNY;
-    public static boolean is_running = false;
-    private static int count = 0;
+    public static boolean is_runningacc = false;
+    private static boolean is_runninggyro = false;
+    private static AtomicInteger countacc = new AtomicInteger(0);
+    private static AtomicInteger countgyr = new AtomicInteger(0);
     private ScheduledExecutorService executorService;
+    private ScheduledExecutorService executorService2;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -127,53 +135,93 @@ public class GpsEtsiit extends Fragment implements SensorEventListener {
         if(event.sensor.getType()==Sensor.TYPE_ACCELEROMETER)
         {
 
-            if(!first_read) {
+            if(!first_read || orientation_change.get()>=20) {
                FCX = event.values[0];
                FCY = event.values[1];
                FCZ = event.values[2];
                SIGNY = Integer.signum((int)FCY);
-                first_read = true;
+               first_read = true;
+               orientation_change.set(0);
+               countacc.set(0);
             }
 
             float y = event.values[1];
             float z = event.values[2];
-            float diff_y = Math.abs(FCY - THRESHOLD_Y);
-            float diff_z = Math.abs(FCZ - THRESHOLD_Z);
+            if(Math.abs(z-lastZ)<=EPSILON_O && Math.abs(y-lastY)<=EPSILON_O)
+                orientation_change.addAndGet(1);
+            else
+                orientation_change.set(0);
+            lastY = y;
+            lastZ = z;
+            float diff_y = Math.abs(FCY - THRESHOLD_Y.get());
+            float diff_z = Math.abs(FCZ - THRESHOLD_Z.get());
             boolean upd_y, upd_z;
-            if(THRESHOLD_Y!=0){
+            if(THRESHOLD_Y.get()!=0){
                 upd_y = Math.abs(y - diff_y) <= EPSILON_Y;
                 upd_z = z + diff_z <= EPSILON_Z;
             }else{
                upd_y = Math.abs(y - FCY) <= EPSILON_Y;
                upd_z = Math.abs(z - FCZ) <= EPSILON_Z;
             }
-            System.out.println(upd_y + " -- " + upd_z + " -- " + is_running + " -- " + count + " -- " + THRESHOLD_Y);
+            System.out.println(upd_y + " -- " + upd_z + " -- " + is_runningacc + " -- " + countacc + " -- " + THRESHOLD_Y + " || " + orientation_change);
             if(upd_y && upd_z){
-                if(is_running && count>=2){
-                    is_running = false;
-                    count += 1;
+                if(is_runningacc && countacc.get()>=3){
+                    is_runningacc = false;
+                    countacc.set(0);
                     Toast.makeText(getContext(),"PATTERN READ",Toast.LENGTH_SHORT).show();
                 }else{
-                    THRESHOLD_Z -= 10f;
-                    THRESHOLD_Y -= 10f;
-                    THRESHOLD_Y = Math.abs(THRESHOLD_Y);
-                    THRESHOLD_Y = Math.abs(THRESHOLD_Z);
-                    count += 1;
-                    if(!is_running) {
+                    THRESHOLD_Z.addAndGet(-10);
+                    THRESHOLD_Y.addAndGet(-10);
+                    THRESHOLD_Y.set(Math.abs(THRESHOLD_Y.get()));
+                    THRESHOLD_Z.set(Math.abs(THRESHOLD_Z.get()));
+                    countacc.getAndIncrement();
+                    if(!is_runningacc) {
                         executorService = Executors.newSingleThreadScheduledExecutor();
-                        executorService.schedule(GpsEtsiit::ResetPattern, 3, TimeUnit.SECONDS);
+                        executorService.schedule(GpsEtsiit::ResetPatternAcc, 3, TimeUnit.SECONDS);
                     }
-                    is_running = true;
+                    is_runningacc = true;
                 }
+            }
+        }
+        if(event.sensor.getType()==Sensor.TYPE_GYROSCOPE)
+        {
+            float y = event.values[1];
+            System.out.println(y);
+            if(Math.signum(THRESHOLD_GY)>=1){
+               if(y>=THRESHOLD_GY) {
+                   countgyr.getAndIncrement();
+                   THRESHOLD_GY *= -1;
+                   if (!is_runninggyro) {
+                       executorService2 = Executors.newSingleThreadScheduledExecutor();
+                       executorService2.schedule(GpsEtsiit::ResetPatternGyro, 3, TimeUnit.SECONDS);
+                   }
+                   is_runninggyro = true;
+               }
+            }else if(countgyr.get()%2==1){
+                if(y<=THRESHOLD_GY) {
+                    countgyr.getAndIncrement();
+                    THRESHOLD_GY *=-1;
+                }
+            }
+            if(is_runninggyro && countgyr.get()>=3){
+                is_runninggyro = false;
+                countgyr.set(0);
+                Toast.makeText(getContext(),"PATTERN 2 READ",Toast.LENGTH_SHORT).show();
             }
         }
     }
 
-    public static void ResetPattern(){
-        is_running = false;
-        THRESHOLD_Y = 10f;
-        THRESHOLD_Z = 10f;
-        count = 0;
+    public static void ResetPatternAcc(){
+        is_runningacc = false;
+        THRESHOLD_Y.set(10);
+        THRESHOLD_Z.set(10);
+        countacc.set(0);
+    }
+
+    public static void ResetPatternGyro(){
+        is_runninggyro = false;
+        THRESHOLD_GY = THRESHOLD_GY*Math.signum(THRESHOLD_GY);
+        countgyr.set(0);
     }
 
     @Override
